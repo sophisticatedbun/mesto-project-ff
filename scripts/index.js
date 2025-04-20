9cc7d18179eed6f1d5610cbcc0be629707da729b
Yuliya Belyaeva <belyu11@yandex.ru>
2025-04-20 23:23:35 +0300

    First version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content; 
 
-// @todo: DOM узлы
+function createCard (initialCard, deleteCallback) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
 
-// @todo: Функция создания карточки
+    cardTitle.textContent = initialCard.name;
+    cardImage.src = initialCard.link;
 
-// @todo: Функция удаления карточки
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+    deleteButton.addEventListener('click', () => { 
+         deleteCallback(cardElement); 
+     }); 
+
+     return cardElement;
+}
+
+function deleteCard (deleteButton) {
+    const closestCard = deleteButton.closest('.card');
+    closestCard.remove();
+}
+
+initialCards.forEach((card) => {
+    placesList.append(createCard(card, deleteCard));
+});
